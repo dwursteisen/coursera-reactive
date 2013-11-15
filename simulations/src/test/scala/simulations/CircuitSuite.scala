35f001c54a417eee1f50c060dcffda920dc2a7bf
David Wursteisen <david.wursteisen+git@gmail.com>
2013-11-15 16:23:51 +0100

    Création des portes binaires

--- simulations/src/test/scala/simulations/CircuitSuite.scala
+++ simulations/src/test/scala/simulations/CircuitSuite.scala
@@ -17,20 +17,69 @@ class CircuitSuite extends CircuitSimulator with FunSuite {
     in1.setSignal(false)
     in2.setSignal(false)
     run
-    
+
     assert(out.getSignal === false, "and 1")
 
     in1.setSignal(true)
     run
-    
+
     assert(out.getSignal === false, "and 2")
 
     in2.setSignal(true)
     run
-    
+
     assert(out.getSignal === true, "and 3")
   }
 
+  test("orGate") {
+    val in1, in2, out = new Wire
+    orGate(in1, in2, out)
+    in1.setSignal(false)
+    in2.setSignal(false)
+    run
+    assert(out.getSignal === false, "and 1")
+
+    in1.setSignal(true)
+    in2.setSignal(false)
+    run
+    assert(out.getSignal === true, "and 2")
+
+    in1.setSignal(false)
+    in2.setSignal(true)
+    run
+    assert(out.getSignal === true, "and 4")
+
+    in1.setSignal(true)
+    in2.setSignal(true)
+    run
+    assert(out.getSignal === true, "and 5")
+
+  }
+
+  test("orGate2") {
+    val in1, in2, out = new Wire
+    orGate2(in1, in2, out)
+    in1.setSignal(false)
+    in2.setSignal(false)
+    run
+    assert(out.getSignal === false, "and 1")
+
+    in1.setSignal(true)
+    in2.setSignal(false)
+    run
+    assert(out.getSignal === true, "and 2")
+
+    in1.setSignal(false)
+    in2.setSignal(true)
+    run
+    assert(out.getSignal === true, "and 4")
+
+    in1.setSignal(true)
+    in2.setSignal(true)
+    run
+    assert(out.getSignal === true, "and 5")
+
+  }
   //
   // to complete with tests for orGate, demux, ...
   //
